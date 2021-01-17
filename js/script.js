fa099f000e88625327d9957168fbe874e0382d2e
Stefan Caky <stefan.caky777@gmail.com>
2021-01-17 19:59:19 +0000

    showpage function
    showage function and I started the addPagination function

--- js/script.js
+++ js/script.js
@@ -2,9 +2,10 @@
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
-function test() {
-   console.log('test')
-}
+console.log(data[1].name.title);
+console.log(data[2].name.title);
+
+
 
 
 /*
@@ -19,7 +20,40 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage (list, page) {
 
+   let startIndex = (page * 9) - 9;
+   let endIndex = page * 9;
+
+   let studentList = document.querySelector('.student-list');
+   studentList.innerHTML = '';
+  
+      for(let i = 0; i <= list.length; i++) {
+         if(i >= startIndex && i < endIndex) {
+                 let element = `  <li class="student-item cf">
+                  <div class="student-details">
+                  <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+                  <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
+                  <span class="email">${list[i].email}</span>
+                  </div>
+                  <div class="joined-details">
+                  <span class="date">${list[i].registered.date}</span>
+                  </div>
+               </li>
+               `
+          //  console.log(element);
+
+            studentList.insertAdjacentHTML('beforeend', element)   
+            
+         }
+        
+        
+      }
+  
+   
+}
+
+showPage(data, 1)
 
 
 /*
@@ -27,6 +61,23 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list) {
 
+   let numberOfPangination = list.length / 9;
+   numberOfPangination = Math.floor(numberOfPangination);
+
+   let linkList = document.querySelector('.link-list');
+   linkList.innerHTML = '';
+
+   for(let i = 1; i <= numberOfPangination; i++) {
+      element = ` <li>
+      <button type="button">${i}</button>
+      </li> `;
+      linkList.insertAdjacentHTML('beforeend', element); 
+   }
+  
+}
+
+addPagination(data);
 
 // Call functions
